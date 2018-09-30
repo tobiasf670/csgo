98f35ea6c07f352e28b3dc442edefc62df3b7536
Tobias Frantsen <tof@amsiq.com>
2018-09-30 12:38:26 +0200

    init

---
+++ csStats/ShowStatsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowStatsViewController.swift
+//  csStats
+//
+//  Created by Tobias Frantsen on 28/09/2018.
+//  Copyright © 2018 Tobias Frantsen. All rights reserved.
+//
+
+import UIKit
+
+class ShowStatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+}
---
+++ csStats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  csStats
+//
+//  Created by Tobias Frantsen on 28/09/2018.
+//  Copyright © 2018 Tobias Frantsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
